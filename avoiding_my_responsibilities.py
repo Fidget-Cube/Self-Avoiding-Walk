709b50b2a554dc5e5c6aa3b65f88279c7f1d19b1
APstarwars <hobbes399@gmail.com>
2022-04-06 17:27:41 -0700

    Add Weight System & Start Column Transitions

--- avoiding_my_responsibilities.py
+++ avoiding_my_responsibilities.py
@@ -2,6 +2,12 @@
 #   Max vonBlankenburg
 #   Zachary Robinson
 
+# Each index is a unique starting column, stores the weight of each
+startTable = [0, 0.5, 1, 1, 2, 2.5, 1, 2, 2.5, 1.5, 2, 2, 2.5, 0.5, 1, 1, 2, 2.5, 1, 2, 2.5, 1.5, 2, 2, 2.5, 0.5, 1, 1, 1, 1, 1.5, 2, 1.5, 2]
+# Transition table for each starting column (there are 27 states)
+startTransitions = [0, 1, 0, 9, 9, 21, 10, 10, 20, 2, 0, 16, 3, 3, 0, 17, 17, 21, 12, 12, 23, 2, 0, 11, 1, 2, 0, 0, 16, 11, 1, 10, 3, 12]
+
+
 # We define our coordinate system as a 3 by infinity grid,
 # with starting point p at any point farthest left (0,-1) (0,0) (0,1)
 # grid goes from (0,-1) to (inf,1)
@@ -16,14 +22,20 @@
 # Every transition will also have a weight, depending on the number of
 # arrows in the character involved in the transition.
 class gridwalk:
-    # Each set contains a state mapped to a weight
-    # The weight is the collective weight of every
-    # transition that led to that particular state
     validSet = [] # In progress
     newSet = [] # In progress
 
     def __init__(self):
-        self.validSet # Fill the first valid set with weights according to the starting table
+        # Each index in validSet represents a DFA state.
+        # There are 27 states, each representing a possible set of paths between each column.
+        # Each value of validSet is an array.
+        # Each index of this array represents a weight.
+        # Each value of this array represents the number of possible paths resulting in that weight.
+        for i in range(27):
+            self.validSet.append([0,0,0,0,0,0]) # Max starting weight = 2.5
+        for i in range(len(startTable)):
+            self.validSet[startTransitions[i]][int(startTable[i] * 2)] += 1
+        print(self.validSet)
     
 
 def main():
